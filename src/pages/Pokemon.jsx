a1a56777261f3136ef94ddd1b4113d045da744a9
Marco Antonio Contreras <70834807+EniDev911@users.noreply.github.com>
2022-10-28 05:05:00 -0300

    Update Pokemon.jsx

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import CardPokemon from "../components/CardPokemon";
 import Loader from "../components/Loading";
 import { useParams } from "react-router-dom";
